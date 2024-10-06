1569afd2f116d2ec02b5b69c1e9fa5930ac74bcf
Ankit Kokane <ankitkokane90@gmail.com>
2024-10-06 16:45:49 +0530

    minor ui update in landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,7 +8,10 @@ export default function LandingPage() {
         <div className="relative flex flex-col justify-center items-center h-[85vh] w-full overflow-clip rounded-[40px]">
           <div className="absolute top-0 bottom-0 left-[60vw] right-0 bg-gradient-to-bl from-[#97a9ab] via-transparent to-transparent opacity-70 h-full" />
           <div className="absolute top-[20vh] bottom-0 left-0 right-0 bg-gradient-to-tr from-[#97a9ab] via-transparent to-transparent opacity-70 h-full" />
-          <Link to="/trade/BTCUSD" className="absolute top-[10vh] left-0 gap-2 text-xl opacity-40">
+          <Link
+            to="/trade/BTCUSD"
+            className="absolute top-[10vh] left-0 gap-2 text-xl opacity-40 hover:opacity-100 transition-all"
+          >
             <img src="/line4.png" alt="" className="w-80 h-20 object-contain" />
             <div className="inline-flex mt-5 ml-10 gap-2 text-base">
               &#8226;
@@ -18,7 +21,10 @@ export default function LandingPage() {
               </div>
             </div>
           </Link>
-          <Link to="/trade/BTCUSD" className="absolute top-[10vh] right-0 gap-2 text-xl opacity-40">
+          <Link
+            to="/trade/BTCUSD"
+            className="absolute top-[10vh] right-0 gap-2 text-xl opacity-40 hover:opacity-100 transition-all"
+          >
             <img src="/line3.png" alt="" className="w-80 h-20 object-contain" />
             <div className="inline-flex mt-5 ml-44 gap-2 text-base">
               &#8226;
@@ -28,7 +34,10 @@ export default function LandingPage() {
               </div>
             </div>
           </Link>
-          <Link to="/trade/BTCUSD" className="absolute bottom-[10vh] left-0 gap-2 text-xl opacity-40">
+          <Link
+            to="/trade/BTCUSD"
+            className="absolute bottom-[10vh] left-0 gap-2 text-xl opacity-40 hover:opacity-100 transition-all"
+          >
             <div className="inline-flex mb-5 ml-10 gap-2 text-base">
               &#8226;
               <div className="">
@@ -38,7 +47,10 @@ export default function LandingPage() {
             </div>
             <img src="/line1.png" alt="" className="w-80 h-20 object-contain" />
           </Link>
-          <Link to="/trade/BTCUSD" className="absolute bottom-[10vh] right-0 gap-2 text-xl opacity-40">
+          <Link
+            to="/trade/BTCUSD"
+            className="absolute bottom-[10vh] right-0 gap-2 text-xl opacity-40 hover:opacity-100 transition-all"
+          >
             <div className="inline-flex mb-5 ml-44 gap-2 text-base">
               &#8226;
               <div className="">
